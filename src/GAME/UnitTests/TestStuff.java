1f8f62fd04832be46113047532d294531b9ab3c0
dot <tgdwarf@gmail.com>
2016-02-23 08:28:06 +0100

    Unit Test?

---
+++ src/GAME/UnitTests/TestStuff.java
@@ -0,0 +1,25 @@
+package GAME.UnitTests;
+
+import GAME.Main;
+import org.junit.Test;
+
+
+/**
+ * Created by dot on 23-02-2016.
+ */
+
+
+public class TestStuff {
+
+    Main main;
+    @org.junit.Test
+    public void TestEnemy() throws Exception {
+
+    }
+
+    @Test
+    public void testSaveGame() throws Exception
+    {
+        Main.save("test");
+    }
+}
